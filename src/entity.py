825b1d4d9b121503cb37a734825c89d93c09df4e
Flaise <x.Flaise@gmail.com>
2018-01-27 10:14:09 -0600

    Basic implementation of entity management

---
+++ src/entity.py
@@ -0,0 +1,26 @@
+
+class Entity(object):
+    def __init__(self):
+        self.components = []
+
+    def get(self, kind):
+        for comp in self.components:
+            if type(comp) == kind:
+                return comp
+        return None
+
+    def add(self, comp):
+        if self.get(type(comp)) is not None:
+            raise TypeError, 'Entity already has component of that type'
+        self.components.append(comp)
+
+def components_typed(entities, kind):
+    for entity in entities:
+        comp = entity.get(kind)
+        if comp is not None:
+            yield comp
+
+def entities_with(entities, kind):
+    for entity in entities:
+        if entity.get(kind) is not None:
+            yield entity
--- src/physics.py
+++ src/physics.py
@@ -1,50 +1,61 @@
 from math import sqrt
+from entity import entities_with
 
-class Entity(object):
+
+class Position(object):
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+
+class Movement(object):
     def __init__(self, radius, mass):
-        self.x = 0
-        self.y = 0
         self.vx = 0
         self.vy = 0
-        self.radius = radius
-        self.mass = mass # float or None
 
-    def __str__(self):
-        return ('Entity[r=' + str(self.radius) + ' x=' + str(self.x) + ' y=' + str(self.y) +
-            ' vx=' + str(self.vx) + ' vy=' + str(self.vy) + ']')
+    # def __str__(self):
+    #     return ('Entity[r=' + str(self.radius) + ' x=' + str(self.x) + ' y=' + str(self.y) +
+    #         ' vx=' + str(self.vx) + ' vy=' + str(self.vy) + ']')
+    #
+    # def __repr__(self):
+    #     return str(self)
 
-    def __repr__(self):
-        return str(self)
+class Collision(object):
+    def __init__(self, mass):
+        self.mass = mass
 
-def distance(x1, y1, x2, y2):
-    dx = x1 - x2
-    dy = y1 - y2
-    return sqrt(dx * dx + dy * dy)
+# def distance(x1, y1, x2, y2):
+#     dx = x1 - x2
+#     dy = y1 - y2
+#     return sqrt(dx * dx + dy * dy)
+#
+# def entity_distance(a, b):
+#     return distance(a.x, a.y, b.x, b.y)
 
-def entity_distance(a, b):
-    return distance(a.x, a.y, b.x, b.y)
-
-def update(entities):
-    for entity in entities:
-        entity.x += entity.vx
-        entity.y += entity.vy
-
-        if entity.mass is None:
-            continue
-        for other in entities:
-            if other == entity or other.mass is None:
-                continue
-            cdist = entity_distance(entity, other)
-            overlap = entity.radius + other.radius - cdist
-            if overlap > 0:
-                entity.vx += abs(entity.x - other.x) / entity.mass / 10
-                entity.vy += abs(entity.y - other.y) / entity.mass / 10
+# def update(entities):
+#     for entity in components_typed(entities, Physics):
+#         entity.x += entity.vx
+#         entity.y += entity.vy
+#
+#         if entity.mass is None:
+#             continue
+#         for other in entities:
+#             if other == entity or other.mass is None:
+#                 continue
+#             cdist = entity_distance(entity, other)
+#             overlap = entity.radius + other.radius - cdist
+#             if overlap > 0:
+#                 entity.vx += abs(entity.x - other.x) / entity.mass / 10
+#                 entity.vy += abs(entity.y - other.y) / entity.mass / 10
 
 def random_entity():
     from random import random
-    result = Entity(random() * 5 + 10, random() * 9 + 1)
-    result.x = random() * 10
-    result.y = random() * 10
+    from entity import Entity
+    # result = Entity(random() * 5 + 10, random() * 9 + 1)
+    # result.x = random() * 10
+    # result.y = random() * 10
+    result = Entity()
+    entity.add(Position(random() * 10, random() * 10))
     return result
 
 ### testing ###
